b2979ac20b69d4d46a158670bdf6f5f30b7fc87f
llpingll <94099281+llpingll@users.noreply.github.com>
2022-11-29 23:59:40 +0000

    Add control function and integrate operate for display

--- script.js
+++ script.js
@@ -1,8 +1,13 @@
+let isOperator = false;
+let fbBuffer = 0;
+let sBuffer = 0;
+
 // Get nodes
 const buttons = document.querySelectorAll("button");
 const feedback = document.querySelector(".feedback");
+const sum = document.querySelector(".sum");
 
-window.addEventListener('DOMContentLoaded', (e) => feedback.textContent = "0");
+window.addEventListener('DOMContentLoaded', () => feedback.textContent = "0");
 
 // Events
 buttons.forEach(button => {
@@ -50,9 +55,31 @@ function controller(e) {
     if (e.target.className === "number") {
         if (feedback.textContent === "0") {
             feedback.textContent = e.target.textContent;
+            fbBuffer = e.target.textContent;
+        }
+        else if (isOperator === true) {
+            feedback.textContent = e.target.textContent;
+            fbBuffer = e.target.textContent;
+            isOperator = false;
         }
         else {
             feedback.textContent += `${e.target.textContent}`;
+            fbBuffer += `${e.target.textContent}`;
         }
+    }
+    else if (e.target.className === "operator") {
+        sum.textContent = `${feedback.textContent} ${e.target.textContent}`;
+        sBuffer = `${feedback.textContent} ${e.target.textContent}`;
+        isOperator = true;
+    }
+    else if (e.target.className === "equal") {
+        isOperator = false;
+        if (!sBuffer.endsWith("=")) {
+            feedback.textContent = operate(sBuffer.split(" ")[1], Number(sBuffer.split(" ")[0]), Number(fbBuffer));
+            fbBuffer = feedback.textContent;
+            sum.textContent += ` ${fbBuffer} =`;
+            sBuffer += ` ${fbBuffer} =`;
+        }
+        
     }
 }
