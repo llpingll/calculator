ae4d26631d26cb8ceb4b03d12173ddec7d913332
llpingll <94099281+llpingll@users.noreply.github.com>
2022-11-29 00:37:28 +0000

    Add event listeners, functions for basic calcs

--- script.js
+++ script.js
@@ -1,3 +1,58 @@
+// Get nodes
+const buttons = document.querySelectorAll("button");
+const feedback = document.querySelector(".feedback");
+
+window.addEventListener('DOMContentLoaded', (e) => feedback.textContent = "0");
+
+// Events
+buttons.forEach(button => {
+    button.addEventListener("click", function(e) {
+        controller(e);
+        // console.log(e.target.class);
+    });
+});
+
+
+// Functions
 function add(a, b) {
-    
+    return a + b;
+}
+
+function subtract(a, b) {
+    return a - b;
+}
+
+function multiply(a, b) {
+    return a * b;
+}
+
+function divide(a, b) {
+    return a / b;
+}
+
+function operate(operator, a, b) {
+    if (operator === "+") {
+        return add(a, b);
+    }
+    else if (operator === "-") {
+        return subtract(a, b);
+    }
+    else if (operator === "*") {
+        return multiply(a, b);
+    }
+    else {
+        return divide(a, b);
+    }
+}
+
+function controller(e) {
+    // console.log(e.target.className);
+    if (e.target.className === "number") {
+        if (feedback.textContent === "0") {
+            feedback.textContent = e.target.textContent;
+        }
+        else {
+            feedback.textContent += `${e.target.textContent}`;
+        }
+    }
 }
